133a15f1c9d3f6e09a2b0ec97f267072ce69bb81
Rituraj Roy <f20170957@goa.bits-pilani.ac.in>
2024-05-14 16:39:21 +0530

    added login page along with validation for email and password

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Header from "./Header";
+import { validate } from "../utils/validate";
 
 const Login = () => {
   const [isSignInForm, setIsSignIsForm] = useState(true);
   const toggleSignInForm = () => {
     setIsSignIsForm(!isSignInForm);
   };
+
+  //to get the reference of email,password and name,we have created useRef
+  const email = useRef(null); //initial value is null
+  const password = useRef(null);
+  const name = useRef(null);
+
+  const [errorMessage, setErrorMeggage] = useState(null);
+
+  const handleButtonClick = () => {
+    console.log(email.current.value);
+    console.log(password.current.value);
+    const message = validate(
+      email.current.value,
+      password.current.value,
+    );
+    console.log(message);
+    setErrorMeggage(message);
+  };
+
   return (
     <div>
       <Header />
@@ -15,23 +35,41 @@ const Login = () => {
           alt="backgroundimg"
         />
       </div>
-      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80"
+      >
         <h1 className="font-bold text-3xl py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
+        {!isSignInForm && (
+          <input
+            //to get the reference of input i.e whatever we type uska reference ref={name}
+            ref={name}
+            type="text"
+            className="p-4 my-2 w-full bg-gray-800 bg-opacity-60"
+            placeholder="Enter your Name"
+          />
+        )}
         <input
+          ref={email}
           type="text"
           className="p-4 my-2 w-full bg-gray-800 bg-opacity-60"
-          placeholder="Email or Mobile Number"
+          placeholder="Email"
         />
         <input
+          ref={password}
           type="password"
           className="p-4 my-2 w-full bg-gray-800 bg-opacity-60"
           placeholder="Password"
         />
-        <button className="p-4 my-6 bg-red-700 w-full rounded-lg hover:bg-red-800">
+        <button
+          className="p-4 my-6 bg-red-700 w-full rounded-lg hover:bg-red-800"
+          onClick={handleButtonClick}
+        >
           {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
+        <p className="text-lg py-2 text-red-500 font-bold">{errorMessage}</p>
         <span className="py-4 text-gray-400">
           {isSignInForm
             ? "New to Netflix?"
