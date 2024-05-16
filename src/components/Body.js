acca6df4865f8305d0c4c6e8951077a517c01db1
Rituraj Roy <f20170957@goa.bits-pilani.ac.in>
2024-05-16 17:13:32 +0530

    added redux store

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Login from "./Login";
 import Browse from "./Browse";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { auth } from "../utils/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { useDispatch } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
 
 const Body = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        //SIGN IN
+        const {uid,email,displayName,photoURL} = user;
+        dispatch(addUser({
+          uid: uid,
+          email:email,
+          displayName: displayName,
+          photoURL: photoURL,
+        })
+      );
+      } else {
+        //SIGN OUT
+        dispatch(removeUser());
+      }
+    });
+  },[]);
+
   const apppRouter = createBrowserRouter([
     {
       path: "/",
--- src/components/Login.js
+++ src/components/Login.js
@@ -4,10 +4,15 @@ import { validate } from "../utils/validate";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  updateProfile,
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Login = () => {
+  const dispatch = useDispatch();
   const [isSignInForm, setIsSignIsForm] = useState(true);
   const toggleSignInForm = () => {
     setIsSignIsForm(!isSignInForm);
@@ -17,6 +22,8 @@ const Login = () => {
   const email = useRef(null); //initial value is null
   const password = useRef(null);
   const name = useRef(null);
+
+  const navigate = useNavigate();
 
   const [errorMessage, setErrorMeggage] = useState(null);
 
@@ -36,6 +43,26 @@ const Login = () => {
           .then((userCredential) => {
             // Signed up
             const user = userCredential.user;
+            updateProfile(user, {
+              displayName: name.current.value,
+              photoURL: "https://avatars.githubusercontent.com/u/49668637?v=4",
+            })
+              .then(() => {
+                //auth.currentuser me new user user profile hoga and user me old wala
+                const { uid, email, displayName, photoURL } = auth.currentUser;
+                dispatch(
+                  addUser({
+                    uid: uid,
+                    email: email,
+                    displayName: displayName,
+                    photoURL: photoURL,
+                  })
+                );
+                navigate("/browse");
+              })
+              .catch((error) => {
+                setErrorMeggage(error.message);
+              });
             console.log(user);
           })
           .catch((error) => {
@@ -55,6 +82,7 @@ const Login = () => {
             // Signed in
             const user = userCredential.user;
             console.log(user);
+            navigate("/browse");
           })
           .catch((error) => {
             const errorCode = error.code;
